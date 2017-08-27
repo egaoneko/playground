ec4f08a5ad220799f055e1c5a19bb19ad913fae1
Donghyun <egaoneko@naver.com>
2017-08-28 00:04:57 +0900

    Add Playground

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Playground from "./Playground";
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello React Skeleton</h1>
+      <Playground/>
     );
   }
 }
---
+++ src/components/Playground.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Playground extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return(
+      <div>
+        Playground
+      </div>
+    );
+  }
+}
+
+export default Playground;
