a7dded40c5b4914ccec5423532e86ca8a5dd3530
Donghyun <egaoneko@naver.com>
2018-09-21 01:02:12 +0900

    feat: Add OL postprocessing example

---
+++ examples/openlayers-postprocessing.js
@@ -0,0 +1,193 @@
+const raster = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+
+const image = new ol.style.Circle({
+  radius: 5,
+  fill: null,
+  stroke: new ol.style.Stroke({color: 'red', width: 1})
+});
+
+const styles = {
+  'Point': new ol.style.Style({
+    image: image
+  }),
+  'LineString': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'green',
+      width: 1
+    })
+  }),
+  'MultiLineString': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'green',
+      width: 1
+    })
+  }),
+  'MultiPoint': new ol.style.Style({
+    image: image
+  }),
+  'MultiPolygon': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'yellow',
+      width: 1
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 0, 0.1)'
+    })
+  }),
+  'Polygon': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'blue',
+      lineDash: [4],
+      width: 3
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(0, 0, 255, 0.1)'
+    })
+  }),
+  'GeometryCollection': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'magenta',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+      color: 'magenta'
+    }),
+    image: new ol.style.Circle({
+      radius: 10,
+      fill: null,
+      stroke: new ol.style.Stroke({
+        color: 'magenta'
+      })
+    })
+  }),
+  'Circle': new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: 'red',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(255,0,0,0.2)'
+    })
+  })
+};
+
+const styleFunction = (feature) => {
+  return styles[feature.getGeometry().getType()];
+};
+
+const geojsonObject = {
+  'type': 'FeatureCollection',
+  'crs': {
+    'type': 'name',
+    'properties': {
+      'name': 'EPSG:3857'
+    }
+  },
+  'features': [{
+    'type': 'Feature',
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [0, 0]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'LineString',
+      'coordinates': [[4e6, -2e6], [8e6, 2e6]]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'LineString',
+      'coordinates': [[4e6, 2e6], [8e6, -2e6]]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'Polygon',
+      'coordinates': [[[-5e6, -1e6], [-4e6, 1e6], [-3e6, -1e6]]]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'MultiLineString',
+      'coordinates': [
+        [[-1e6, -7.5e5], [-1e6, 7.5e5]],
+        [[1e6, -7.5e5], [1e6, 7.5e5]],
+        [[-7.5e5, -1e6], [7.5e5, -1e6]],
+        [[-7.5e5, 1e6], [7.5e5, 1e6]]
+      ]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'MultiPolygon',
+      'coordinates': [
+        [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6], [-3e6, 6e6]]],
+        [[[-2e6, 6e6], [-2e6, 8e6], [0, 8e6], [0, 6e6]]],
+        [[[1e6, 6e6], [1e6, 8e6], [3e6, 8e6], [3e6, 6e6]]]
+      ]
+    }
+  }, {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'GeometryCollection',
+      'geometries': [{
+        'type': 'LineString',
+        'coordinates': [[-5e6, -5e6], [0, -5e6]]
+      }, {
+        'type': 'Point',
+        'coordinates': [4e6, -5e6]
+      }, {
+        'type': 'Polygon',
+        'coordinates': [[[1e6, -6e6], [2e6, -4e6], [3e6, -6e6]]]
+      }]
+    }
+  }]
+};
+
+const vectorSource = new ol.source.Vector({
+  features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
+});
+
+vectorSource.addFeature(new ol.Feature(new ol.geom.Circle([5e6, 7e6], 1e6)));
+
+const vector = new ol.layer.Vector({
+  source: vectorSource,
+  style: styleFunction
+});
+
+const map = new ol.Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+//Apply a filter on "postcompose" events.
+raster.on('postcompose', function(event) {
+  console.log(event);
+  greyscale(event.context);
+});
+
+function greyscale(context) {
+  const canvas = context.canvas;
+  const ctx = canvas.getContext('2d');
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for(var i = 0; i < data.length; i += 4) {
+    var brightness = (data[i] + data[i+1] + data[i+2]) / 3;
+    // red
+    data[i] = brightness;
+    // green
+    data[i+1] = brightness;
+    // blue
+    data[i+2] = brightness;
+  }
+  ctx.putImageData(imageData, 0, 0);
+}
