827d03a4a166e16a364e44213c2e47af0797551f
donghyun <egaoneko@naver.com>
2018-09-25 15:19:07 +0900

    feat: Add tabindex page

---
+++ docs/examples/tabindex.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{21:function(n,e){document.getElementById("test").onclick=function(){document.getElementById("scripted").focus()}}},[[21,0]]]);
+//# sourceMappingURL=tabindex.js.map
---
+++ examples/tabindex.js
@@ -0,0 +1,3 @@
+document.getElementById('test').onclick = function () {
+  document.getElementById('scripted').focus();
+};
