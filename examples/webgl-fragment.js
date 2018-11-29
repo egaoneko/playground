29bb49c26ed28f76d9967447854940ef7b4ad9b2
Donghyun <egaoneko@naver.com>
2018-11-29 23:04:55 +0900

    feat: edit webgl fragment

--- examples/webgl-fragment.js
+++ examples/webgl-fragment.js
@@ -26,6 +26,10 @@ const SHADER = {
     vertexShader: circleUniform2VertexShaderSource(),
     fragmentShader: circleUniform2FragmentShaderSource()
   },
+  mandelbrot:  {
+    vertexShader: mandelbrotVertexShaderSource(),
+    fragmentShader: mandelbrotFragmentShaderSource()
+  },
 };
 
 const radios = document.querySelectorAll('input[type=radio][name="fragment"]');
@@ -276,6 +280,44 @@ function circleUniform2FragmentShaderSource() {
       highp float x = sin(-position.y) * tan(length(position.xx));
       t = t + x;
       gl_FragColor = mix( vec4(0.0, 0.0, 0.0, 1.0), vec4(texture2D(sColors, vec2(0.0, t)).rgb, 1.0), t);
+    }
+  `;
+}
+
+function mandelbrotVertexShaderSource() {
+  return `
+    attribute vec3 aVertexPosition;
+    varying vec2 position;
+    
+    void main(void) {
+      position = vec2(aVertexPosition.xy);
+      gl_Position = vec4(position, 0.0, 1.0);
+    }
+  `;
+}
+
+function mandelbrotFragmentShaderSource() {
+  return `
+    varying highp vec2 position;
+    const int MAX_ITERATIONS = 250;
+    const highp float LIGHTNESS_FACTOR = 10.0;
+    
+    void main(void) {   
+      highp vec2 c = vec2(position.x - 0.5, position.y); 
+      highp vec2 z = c;
+      
+      highp vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+      
+      for (int i = 0; i < MAX_ITERATIONS; i++) {
+        z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+        
+        if (dot(z, z) > 4.0) {
+          highp float f = LIGHTNESS_FACTOR * float(i) / float(MAX_ITERATIONS);
+          color = vec4(vec3(0.1, 0.1, 1.0) * f , 1.0);
+          break;
+        }
+      }
+      gl_FragColor = color;
     }
   `;
 }
