b33e484aa0811efd70ced7e5959512c88ddb3abf
Donghyun Seo <donghyun.seo@navercorp.com>
2019-02-07 14:05:16 +0900

    feat: Edit openlayer turf

--- examples/openlayers-turf.js
+++ examples/openlayers-turf.js
@@ -1,35 +1,119 @@
-const coordinates = [
+let coordinates;
+
+// coordinates = [
+//   [
+//     127.09610939025879,
+//     37.351464686498815
+//   ],
+//   [
+//     127.10108757019043,
+//     37.35214697629814
+//   ],
+//   [
+//     127.10505723953246,
+//     37.354739620980915
+//   ],
+//   [
+//     127.10668802261353,
+//     37.35787796578827
+//   ],
+//   [
+//     127.10939168930054,
+//     37.35895247760407
+//   ],
+//   [
+//     127.11222410202026,
+//     37.36563798323926
+//   ],
+//   [
+//     127.11662292480467,
+//     37.352129919128714
+//   ],
+//   [
+//     127.11984157562254,
+//     37.358730754203336
+//   ]
+// ];
+
+// coordinates = [
+//   [
+//     127.09726810455322,
+//     37.35509780827176
+//   ],
+//   [
+//     127.10087299346922,
+//     37.36492170755341
+//   ],
+//   [
+//     127.10422039031981,
+//     37.35796324506581
+//   ],
+//   [
+//     127.10692405700685,
+//     37.352419890481514
+//   ],
+//   [
+//     127.11267471313475,
+//     37.35020243402261
+//   ],
+//   [
+//     127.11718082427977,
+//     37.35721278409788
+//   ],
+//   [
+//     127.11949825286865,
+//     37.36285811318619
+//   ],
+//   [
+//     127.12260961532593,
+//     37.364068989488395
+//   ],
+//   [
+//     127.12679386138916,
+//     37.354364375319165
+//   ]
+// ];
+
+coordinates = [
   [
-    127.09610939025879,
-    37.351464686498815
+    127.09359884262085,
+    37.35185700389112
   ],
   [
-    127.10108757019043,
-    37.35214697629814
+    127.09507942199708,
+    37.35815085913536
   ],
   [
-    127.10505723953246,
-    37.354739620980915
+    127.09840536117554,
+    37.364614730166345
   ],
   [
-    127.10668802261353,
-    37.35787796578827
+    127.10488557815552,
+    37.35966881026814
   ],
   [
-    127.10939168930054,
-    37.35895247760407
+    127.10492849349976,
+    37.35554127588281
   ],
   [
-    127.11222410202026,
-    37.36563798323926
+    127.1119236946106,
+    37.35364799202858
   ],
   [
-    127.11662292480467,
-    37.352129919128714
+    127.11548566818237,
+    37.361459612015395
   ],
   [
     127.11984157562254,
-    37.358730754203336
+    37.366746491654546
+  ],
+  [
+    127.1242833137512,
+    37.35941297795872
+  ],
+  [
+    127.12857484817505,
+    37.35567772692831
   ]
 ];
 
@@ -68,6 +152,14 @@ const line = turf.lineString(coordinates);
 const bbox = turf.bbox(line);
 const bboxPolygon = turf.bboxPolygon(bbox);
 
+const mid = [(bbox[0] + bbox[2]) * 0.5, (bbox[1] + bbox[3]) * 0.5];
+const lines = [
+  [[bbox[0], bbox[1]], mid],
+  [[bbox[2], bbox[3]], mid],
+  [[bbox[2], bbox[1]], mid],
+  [[bbox[0], bbox[3]], mid]
+];
+
 const vector2 = new ol.layer.Vector({
   source: new ol.source.Vector({
     features: (new ol.format.GeoJSON()).readFeatures(bboxPolygon)
@@ -87,24 +179,16 @@ const vector2 = new ol.layer.Vector({
 
 geojson = {
   'type': 'FeatureCollection',
-  'features': [
-    {
+  'features': lines.map(line => {
+    return {
       'type': 'Feature',
       'properties': {},
       'geometry': {
         'type': 'LineString',
-        'coordinates': [[bbox[0], bbox[3]], [bbox[2], bbox[1]]]
+        'coordinates': line
       }
-    },
-    {
-      'type': 'Feature',
-      'properties': {},
-      'geometry': {
-        'type': 'LineString',
-        'coordinates': [[bbox[0], bbox[1]], [bbox[2], bbox[3]]]
-      }
-    }
-  ]
+    };
+  }),
 };
 
 const vector3 = new ol.layer.Vector({
@@ -122,14 +206,15 @@ const vector3 = new ol.layer.Vector({
 });
 
 const origin = turf.lineString(coordinates);
-const line1 = turf.lineString([[bbox[0], bbox[3]], [bbox[2], bbox[1]]]);
-const line2 = turf.lineString([[bbox[0], bbox[1]], [bbox[2], bbox[3]]]);
-const intersects1 = turf.lineIntersect(origin, line1);
-const intersects2 = turf.lineIntersect(origin, line2);
 
 let collectionFeatures = [];
-collectionFeatures = collectionFeatures.concat(intersects1.features);
-collectionFeatures = collectionFeatures.concat(intersects2.features);
+collectionFeatures = collectionFeatures.concat(lines.map(line => {
+    return turf.lineIntersect(origin, turf.lineString(line))
+  })
+    .reduce((prev, current) => {
+      return prev.concat(current.features);
+    }, [])
+);
 
 function getBBoxOnLine(coordinates) {
   const size = coordinates.length;
