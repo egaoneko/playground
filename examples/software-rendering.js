eaf684471193222ccbb52a0e54abd23af2d91c96
Donghyun Seo <donghyun.seo@navercorp.com>
2019-08-11 22:19:54 +0900

    Edit

--- examples/software-rendering.js
+++ examples/software-rendering.js
@@ -1,5 +1,4 @@
 class Matrix4 {
-  
   static create() {
     const out = new Float32Array(16);
     out[0] = 1;
@@ -10,15 +9,39 @@ class Matrix4 {
   }
 
   static multiply(out, a, b) {
-    const a00 = a[0], a01 = a[1],  a02 = a[2], a03 = a[3];
-    const a10 = a[4], a11 = a[5], a12 = a[6], a13 = a[7];
-    const a20 = a[8], a21 = a[9], a22 = a[10], a23 = a[11];
-    const a30 = a[12], a31 = a[13], a32 = a[14], a33 = a[15];
+    const a00 = a[0],
+      a01 = a[1],
+      a02 = a[2],
+      a03 = a[3];
+    const a10 = a[4],
+      a11 = a[5],
+      a12 = a[6],
+      a13 = a[7];
+    const a20 = a[8],
+      a21 = a[9],
+      a22 = a[10],
+      a23 = a[11];
+    const a30 = a[12],
+      a31 = a[13],
+      a32 = a[14],
+      a33 = a[15];
 
-    const b00 = b[0], b01 = b[1],  b02 = b[2], b03 = b[3];
-    const b10 = b[4], b11 = b[5], b12 = b[6], b13 = b[7];
-    const b20 = b[8], b21 = b[9], b22 = b[10], b23 = b[11];
-    const b30 = b[12], b31 = b[13], b32 = b[14], b33 = b[15];
+    const b00 = b[0],
+      b01 = b[1],
+      b02 = b[2],
+      b03 = b[3];
+    const b10 = b[4],
+      b11 = b[5],
+      b12 = b[6],
+      b13 = b[7];
+    const b20 = b[8],
+      b21 = b[9],
+      b22 = b[10],
+      b23 = b[11];
+    const b30 = b[12],
+      b31 = b[13],
+      b32 = b[14],
+      b33 = b[15];
 
     out[0] = a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30;
     out[1] = a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31;
@@ -65,7 +88,7 @@ class Matrix4 {
     if (far != null && far !== Infinity) {
       nf = 1 / (near - far);
       out[10] = (far + near) * nf;
-      out[14] = (2 * far * near) * nf;
+      out[14] = 2 * far * near * nf;
     } else {
       out[10] = -1;
       out[14] = -2 * near;
@@ -75,14 +98,52 @@ class Matrix4 {
   }
 }
 
+class Vector4 {
+  static create() {
+    const out = new Float32Array(4);
+    out[0] = 0;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 0;
+    return out;
+  }
+
+  static fromValues(x, y, z, w) {
+    const out = new Float32Array(4);
+    out[0] = x;
+    out[1] = y;
+    out[2] = z;
+    out[3] = w;
+    return out;
+  }
+
+  static transformMat4(out, a, m) {
+    const x = a[0],
+      y = a[1],
+      z = a[2],
+      w = a[3];
+    out[0] = m[0] * x + m[4] * y + m[8] * z + m[12] * w;
+    out[1] = m[1] * x + m[5] * y + m[9] * z + m[13] * w;
+    out[2] = m[2] * x + m[6] * y + m[10] * z + m[14] * w;
+    out[3] = m[3] * x + m[7] * y + m[11] * z + m[15] * w;
+    return out;
+  }
+}
+
+class Model {
+  constructor(position) {
+    this.position = position;
+  }
+}
+
 class Renderer {
   constructor(container) {
-    if (typeof container === 'string') {
+    if (typeof container === "string") {
       container = document.querySelector(`#${container}`);
     }
 
     if (!(container instanceof HTMLElement)) {
-      throw 'Container must be HTMLElment';
+      throw "Container must be HTMLElment";
     }
 
     this._initCanvas(container);
@@ -90,21 +151,36 @@ class Renderer {
 
   draw() {
     this.ctx.fillRect(20, 20, 20, 20);
+    const model = new Model(Vector4.fromValues(10, 10, 1, 1));
+    const mMatrix = Matrix4.create();
+    const vMatrix = Matrix4.create();
+    const pMatrix = Matrix4.perspective(
+      Matrix4.create(),
+      45,
+      this.width / this.height,
+      0.1,
+      100.0
+    );
+
+    const vector = Vector4.transformMat4(Vector4.create(), model.position, mMatrix);
+    Vector4.transformMat4(vector, vector, vMatrix);
+    Vector4.transformMat4(vector, vector, pMatrix);
+    console.log(vector);
   }
 
   _initCanvas(container) {
     this.width = container.clientWidth;
     this.height = container.clientHeight;
-    const canvas = document.createElement('canvas');
+    const canvas = document.createElement("canvas");
     canvas.width = this.width;
     canvas.height = this.height;
 
     this.canvas = canvas;
-    this.ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext("2d");
 
     container.appendChild(canvas);
   }
 }
 
-const renderer = new Renderer('container');
+const renderer = new Renderer("container");
 renderer.draw();
